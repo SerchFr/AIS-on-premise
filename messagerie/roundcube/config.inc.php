ccfbd6624d4c25b558485fefb0f3372b59e1adb7
Sergio <sergio.martinez@le-campus-numerique.fr>
2026-08-12 03:35:41 +0200

    certificates for the email

--- messagerie/roundcube/config.inc.php
+++ messagerie/roundcube/config.inc.php
@@ -2,17 +2,14 @@
 $config['default_host'] = 'ssl://imap.alpesnet.local';
 #$config['default_host'] = 'dovecot';
 $config['default_port'] = 143;
-#$config['default_port'] = 993;
 
 $config['smtp_server'] = 'tls://smtp.alpesnet.local';
 #$config['smtp_server'] = 'mail.alpesnet.local';
-#$config['smtp_port'] = 587;
 $config['smtp_port'] = 25;
 $config['smtp_user'] = '%u';
 $config['smtp_pass'] = '%p';
 
+$config['force_https'] = true;
+$config['use_https'] = true;
 
-#$config['smtp_host'] = 'postfix:25';
 
-
-#$config['imap_host'] = 'dovecot:143';
